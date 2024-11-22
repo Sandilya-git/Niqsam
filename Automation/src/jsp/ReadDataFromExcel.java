cd12918249e461a150b4b4c69feeb1782361632f
umigithb <161281630+umigithb@users.noreply.github.com>
2024-11-22 16:13:27 +0530

    Code from Umesh

--- Automation/src/jsp/ReadDataFromExcel.java
+++ Automation/src/jsp/ReadDataFromExcel.java
@@ -3,19 +3,13 @@ package jsp;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import org.apache.poi.EncryptedDocumentException;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 
 public class ReadDataFromExcel {
 
-	public static void main(String[] args) throws EncryptedDocumentException, IOException {
+	public static void main(String[] args) {
 
-		FileInputStream fis= new FileInputStream("./data/Exceldata.xlsx");
-	    Workbook wb = WorkbookFactory.create(fis);
-	   String str = wb.getSheet("CreateCustomer").getRow(1).getCell(3).getStringCellValue();
-	    System.out.println(str);
-	    
+		
 	}
 
 }
--- Automation/src/jsp/UseGenericLib.java
+++ Automation/src/jsp/UseGenericLib.java
@@ -5,14 +5,11 @@ import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.actitime.generic.FileLib;
 
 public class UseGenericLib {
 
 	public static void main(String[] args) throws IOException {
-		FileLib fl=new FileLib();
-	WebDriver driver = new ChromeDriver();
-	driver.get(fl.getProperty("url"));
+	
 
 	}
 
